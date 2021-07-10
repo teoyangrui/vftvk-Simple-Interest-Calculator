18da32a8ac6d9ec4ee2547b700b05cab56439fbd
teoyangrui <37584702+teoyangrui@users.noreply.github.com>
2021-07-10 23:50:58 +0800

    Final submission

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 function compute()
 {
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal*years*rate/100
-var year=new Date().getFullYear()+parseInt(years);
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = principal * years * rate /100
+  var year= new Date().getFullYear()+parseInt(years);
+  console.log(year)
+  if(year <2022) {
+      alert("Please enter a proper year")
+      return false
+  }
+  if(principal<=0) {
+      alert("Please enter a positive number")
+      principal.focus()
+      return false
 
-if((year != '[object Date]') ||(year <2022)) {
-    alert("Please enter a proper year")
-    return false
-}
-if(principal<=0) {
-    alert("Please enter a positive number")
-    principal.focus()
-    return false
-
-}
-document.getElementById("result").innerText="If you deposite <mark>"+String(principal)
-+"</mark> <br\> at an interest rate of 3.5% <br\> You will receive an amount of <mark>"+String(interest)+
-"</mark> <br\> in the year "+String(year)
-return True
+  }
+  document.getElementById("result").innerHTML=" <br>If you deposite <mark>"+String(principal)
+  +"</mark> <br\> at an interest rate of <mark>"+ String(rate) +"</mark> <br\> You will receive an amount of <mark>"+ String(interest)+ 
+  "</mark> <br\> in the year <mark>"+String(year)+"</mark>"
 }
 
 function updateRate()
 {
   var rateval = document.getElementById("rate").value;
-  document.getElementById("rate_val").innerText=rateval;}
+  document.getElementById("rate_val").innerText=rateval+"%";
+  }
